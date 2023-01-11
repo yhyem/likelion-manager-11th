cf2e032a3bc6454ab636a6124d8a885ef12640ea
yhyem <yoonhyemin0120@gmail.com>
2023-01-11 22:58:43 +0900

    feat: 현재 player 표시

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,14 @@ import React from 'react';
 import '../styles/Square.css'
 import { useState } from 'react';
 
-const Square = () => {
+const Square = ({ data, index, result, user }) => {
     //기능 구현전 간단하게 값 세팅
     const [value, setValue] = useState('');
     return (
-        <button className="square" onClick={() => setValue('X')}>
+        <button className="square" onClick={() => {
+            data == null && setValue(user ? 'O' : 'X');
+            data == null && result(user ? 'O' : 'X', index);
+        }}>
             {value}
         </button>
     );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Table.css'
 import Square from './Square';
 
 const Table = () => {
+    const [array, setArray] = useState([null, null, null, null, null, null, null, null, null]);
+    const [isNext, setIsNext] = useState(true);
+
+    const handleClick = (value, index) => {
+        array[index] = value;
+        setArray(...[array]);
+        setIsNext(!isNext);
+        console.log(array);
+    }
+
     return (
         <div className="table">
-            <div className="board">
-                <Square />
-                <Square />
-                <Square />
-            </div>
-            <div className="board">
-                <Square />
-                <Square />
-                <Square />
-            </div>
-            <div className="board">
-                <Square />
-                <Square />
-                <Square />
-            </div>
+            <div>현재 player : {isNext ? 'O' : 'X'}</div>
+            {array.map((value, index) => (
+                <div className={(index + 1) % 3 === 0 ? "board" : ''} key={index}>
+                    <Square data={value} index={index} result={handleClick} user={isNext} />
+                </div>
+            )
+            )}
+
+
         </div>
     );
 };
