ecde4071771c9d51949a305d678a9806e3b2fed8
yhyem <yoonhyemin0120@gmail.com>
2023-01-11 23:25:22 +0900

    feat: 승자 결정 기능 추가

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,11 +7,36 @@ const Table = () => {
     const [isNext, setIsNext] = useState(true);
 
     const handleClick = (value, index) => {
+        if (calculateWinner(array) || array[index]) {
+            setArray([null, null, null, null, null, null, null, null, null]);
+            return;
+        }
         array[index] = value;
         setArray(...[array]);
         setIsNext(!isNext);
-        console.log(array);
     }
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const winner = calculateWinner(array);
 
     return (
         <div className="table">
@@ -22,8 +47,7 @@ const Table = () => {
                 </div>
             )
             )}
-
-
+            <div>{winner ? 'Winner : ' + winner : null}</div>
         </div>
     );
 };
